dc341d40b784222e968b09d59258573a4e2d1599
Pavel <balobanov057@gmail.com>
2021-07-20 22:27:44 +0400

    MyBestFriends
    Переработка блока друзей на базе js/jQuery с использованием Json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Драггинг
-$(function () {
+$(document).ready(() => {
     $(".drag").draggable({ containment: "#info" });
 });
 
@@ -8,21 +8,54 @@ $(window).on('load', () => {
     window.setTimeout(() => {
         $('#preloader').fadeOut('slow');
     }, 1600);
-    window.setTimeout(() => {$('#preloader').remove()}, 2000)
+    window.setTimeout(() => { $('#preloader').remove() }, 2000)
 })
 
 //Печать
-$(function()
-{
+$(document).ready(() => {
 
-        $(".typed").typed({
-            strings: [" niggers.", " feminism.", " LGBT."],
-            typeSpeed: 50,
-            backDelay: 1000,
-            startDelay: 500,
-            loop: true,
-            contentType: 'html',      
-        });
+    $(".typed").typed({
+        strings: [" niggers.", " feminism.", " LGBT."],
+        typeSpeed: 50,
+        backDelay: 1000,
+        startDelay: 500,
+        loop: true,
+        contentType: 'html',
+    });
 
 }
-);
+)
+
+const pathImgData = "/js/data/pathImg.json";
+var innerHtmlCont = '', tempHtml, idFriends = ['collapseEgor', 'collapseDanil', 'collapseArtem', 'collapseAlex'];
+
+$(window).on('load', () => {
+    let id = 0;
+    $.getJSON(pathImgData, function (data) {
+        data = data[0];
+        Object.keys(data).forEach(key => {
+            tempHtml = '';
+            Object.keys(data[key]).forEach(value => {
+                tempHtml += `
+                <div class="col-sm">
+                    <div class="card">
+                        <img src="media/${idFriends[id].split(/(?=[A-Z])/)[1] + '/' + data[key][value]}" 
+                            class="card-img-top" alt="">
+                    </div>
+                </div>`
+            });
+            innerHtmlCont +=
+                `
+                <div class="col-lg">
+                    <div class="collapse" id="${idFriends[id]}">
+                        <div class="row row-cols-sm-3 gy-4 justify-content-evenly">
+                            ${tempHtml}
+                        </div>
+                    </div>
+                </div>
+                `
+            id++;
+        });
+        document.getElementById('bodyInner').insertAdjacentHTML('beforeend', innerHtmlCont);
+    });
+})
