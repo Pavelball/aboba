1a3a39ed4586bbd5aa7abef92d94107d55f13145
Pavel <balobanov057@gmail.com>
2021-07-20 13:45:51 +0400

    My Best Friends

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
+//Драггинг
 $(function () {
     $(".drag").draggable({ containment: "#info" });
 });
 
-$(function () {
-    $("#sortable").sortable({
-        revert: true
-    });
-    $("ul, li").disableSelection();
-});
+//Прелоадер
+$(window).on('load', () => {
+    window.setTimeout(() => {
+        $('#preloader').fadeOut('slow');
+    }, 1600);
+    window.setTimeout(() => {$('#preloader').remove()}, 2000)
+})
+
+//Печать
+$(function()
+{
+
+        $(".typed").typed({
+            strings: [" niggers.", " feminism.", " LGBT."],
+            typeSpeed: 50,
+            backDelay: 1000,
+            startDelay: 500,
+            loop: true,
+            contentType: 'html',      
+        });
+
+}
+);
